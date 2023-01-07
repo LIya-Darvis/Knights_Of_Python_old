14a949278c2e929fabfe52eedec2d1638759567c
Liya Darvis <anastasiaadm2019@gmail.com>
2023-01-08 01:00:53 +0300

    camera refactoring

--- map/main.js
+++ map/main.js
@@ -13,6 +13,5 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(30);
-camera.position.setX(-3);
 
 renderer.render(scene, camera);
