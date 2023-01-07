7878e93fe5a3d05248637d6dd9a34641dc8b9f38
Liya Darvis <anastasiaadm2019@gmail.com>
2023-01-08 00:50:51 +0300

    render adding

--- map/main.js
+++ map/main.js
@@ -1,4 +1,4 @@
-import './map_style.css'
+import '..//map/map_style.css';
 
 import * as THREE from 'three';
 
@@ -7,12 +7,12 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 const renderer = new THREE.WebGLRenderer({
-    canvas: document.querySelector('#bg'),
-})
+  canvas: document.querySelector('#bg'),
+});
 
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(30);
+camera.position.setX(-3);
 
 renderer.render(scene, camera);
-
